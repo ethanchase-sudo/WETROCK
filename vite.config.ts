159e36790d89561c6c43c17e931d7f12ec6f4254
ethanchase-sudo <ethanchase@digitalsoftwarelabs.com>
2026-08-18 23:05:26 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/WETROCK-main/', // Replace WETROCK-main with your exact GitHub repository name
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
